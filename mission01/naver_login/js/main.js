9d45c98f800a24c7db5ef73867c4f57ab02856af
bambiru <dj70083798@gmail.com>
2023-12-08 17:15:07 +0900

    - email, password 정규표현식을 사용한 validation

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,36 +1,48 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
-
 1. email 정규표현식을 사용한 validation
 2. pw 정규표현식을 사용한 validation
 3. 상태 변수 관리
 4. 로그인 버튼을 클릭시 조건처리
-
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const userEmail = document.querySelector("#userEmail");
+const userPassword = document.querySelector("#userPassword");
 
-  return re.test(String(text).toLowerCase())
-}
+let email = "";
+userEmail.addEventListener("input", (event) => {
+  email = event.target.value;
 
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  if (emailReg(email)) {
+    userEmail.classList.remove("is--invalid");
+  } else {
+    userEmail.classList.add("is--invalid");
+  }
+});
+
+let password = "";
+userPassword.addEventListener("input", (event) => {
+  password = event.target.value;
+
+  if (pwReg(password)) {
+    userPassword.classList.remove("is--invalid");
+  } else {
+    userPassword.classList.add("is--invalid");
+  }
+});
+
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   return re.test(String(text).toLowerCase());
 }
 
-
-
-
-
-
-
-
-
-
-
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
